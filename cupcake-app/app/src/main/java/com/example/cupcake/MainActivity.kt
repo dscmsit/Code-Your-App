44d5af0c2baa7ba7e6f7b98b3b79437ea1787a5e
lakshay6907 <95403606+lakshay6907@users.noreply.github.com>
2022-10-24 17:54:04 +0530

    Update MainActivity.kt

--- cupcake-app/app/src/main/java/com/example/cupcake/MainActivity.kt
+++ cupcake-app/app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -30,6 +30,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_main)
 
+        //nav graph is a place where aur fragments are organised and used
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
